6261926ed82afffe3d39154457879af2924fbc69
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-06-13 17:50:18 +0200

    Replace module service URL with call to service discovery

--- src/main/java/io/archilab/prox/projectservice/module/StudyCourseClient.java
+++ src/main/java/io/archilab/prox/projectservice/module/StudyCourseClient.java
@@ -1,5 +1,7 @@
 package io.archilab.prox.projectservice.module;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -7,9 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
@@ -21,31 +21,34 @@ import org.springframework.stereotype.Component;
 
 
 @Component
+@Slf4j
 public class StudyCourseClient {
 
   private static final String[] filteredModuleNames =
       new String[] {"Master Thesis", "Masterarbeit", "Bachelor", "Praxisprojekt"};
+  private final EurekaClient eurekaClient;
 
-  private final Logger logger = LoggerFactory.getLogger(StudyCourseClient.class);
+  public StudyCourseClient(EurekaClient eurekaClient) {
+    this.eurekaClient = eurekaClient;
+  }
 
-  @Value("${module.service.url}")
-  private String moduleServiceURL;
-
-
-  public StudyCourseClient() {}
+  private String serviceUrl() {
+    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("module-service", false);
+    return instance.getHomePageUrl();
+  }
 
   private Traverson getTraversonInstance(String url) {
     try {
       return new Traverson(new URI(url), MediaTypes.HAL_JSON);
     } catch (URISyntaxException e) {
-      this.logger.error("Could not init Traverson");
+      StudyCourseClient.log.error("Could not init Traverson");
       e.printStackTrace();
       return null;
     }
   }
 
   public List<StudyCourse> getStudyCourses() {
-    Traverson traverson = this.getTraversonInstance(this.moduleServiceURL);
+    Traverson traverson = this.getTraversonInstance(this.serviceUrl());
     if (traverson == null) {
       return new ArrayList<>();
     }
@@ -95,7 +98,7 @@ public class StudyCourseClient {
       }
     } catch (Exception e) {
       e.printStackTrace();
-      this.logger.error("Error retrieving modules");
+      StudyCourseClient.log.error("Error retrieving modules");
     }
 
     return studyCourses;
@@ -116,18 +119,18 @@ public class StudyCourseClient {
   /*
    * public List<StudyCourse> getStudyCourses() { // Test-method which creates studyCourses try {
    * List<StudyCourse> studyCourses = new ArrayList<>();
-   * 
+   *
    * int coursesC = 50; int modulesC = 10; int moduleID = 0; for (int i = 0; i < coursesC; i++) {
    * StudyCourse sc = new StudyCourse(); sc.setAcademicDegree(AcademicDegree.MASTER);
    * sc.setExternalStudyCourseID(new ExternalStudyCourseID(new
    * URL("http://localhost:9002/studyCourses/" + i))); sc.setName(new StudyCourseName("Studiengang "
    * + i));
-   * 
+   *
    * List<Module> modules = new ArrayList<>(); for (int j = 0; j < modulesC; j++) { Module module =
    * new Module(); module.setExternalModuleID(new ExternalModuleID(new
    * URL("http://localhost:9002/modules/" + moduleID))); module.setName(new ModuleName("Modul " +
    * moduleID)); moduleID++; modules.add(module); } sc.setModules(modules); studyCourses.add(sc); }
-   * 
+   *
    * return studyCourses; } catch (Exception e) { return new ArrayList<>(); } }
    */
 }
--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -1,39 +1,62 @@
-CREATE TABLE IF NOT EXISTS "project-db".module (
-                                   id uuid NOT NULL,
-                                   self_ref character varying(255),
-                                   name character varying(255)
+create table module
+(
+    id       uuid not null,
+    self_ref varchar(255),
+    name     varchar(255),
+    primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS "project-db".module_courses (
-                                           study_course_id uuid NOT NULL,
-                                           module_id uuid NOT NULL
+create table project
+(
+    id              uuid not null,
+    created         timestamp,
+    creatorid       uuid,
+    creator_name    varchar(255),
+    description     varchar(3000),
+    modified        timestamp,
+    name            varchar(255),
+    status          int4,
+    supervisor_name varchar(255),
+    primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS "project-db".project (
-                                    id uuid NOT NULL,
-                                    created timestamp without time zone,
-                                    creatorid uuid,
-                                    creator_name character varying(255),
-                                    description character varying(3000),
-                                    modified timestamp without time zone,
-                                    name character varying(255),
-                                    status integer,
-                                    supervisor_name character varying(255)
+create table project_modules
+(
+    project_id uuid not null,
+    modules_id uuid not null
 );
 
-CREATE TABLE IF NOT EXISTS "project-db".project_modules (
-                                            project_id uuid NOT NULL,
-                                            modules_id uuid NOT NULL
+create table study_course
+(
+    id              uuid not null,
+    academic_degree int4,
+    self_ref        varchar(255),
+    name            varchar(255),
+    primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS "project-db".study_course (
-                                         id uuid NOT NULL,
-                                         academic_degree integer,
-                                         self_ref character varying(255),
-                                         name character varying(255)
+create table study_course_modules
+(
+    study_course_id uuid not null,
+    modules_id      uuid not null
 );
 
-CREATE TABLE IF NOT EXISTS "project-db".study_course_modules (
-                                                 study_course_id uuid NOT NULL,
-                                                 modules_id uuid NOT NULL
-);
+alter table study_course_modules
+    add constraint UK_study_course_modules_modules_id
+        unique (modules_id);
+
+alter table project_modules
+    add constraint FK_project_modules_module
+        foreign key (modules_id) references module;
+
+alter table project_modules
+    add constraint FK_project_modules_project
+        foreign key (project_id) references project;
+
+alter table study_course_modules
+    add constraint FK_study_course_modules_module
+        foreign key (modules_id) references module;
+
+alter table study_course_modules
+    add constraint FK_study_course_modules_study_course
+        foreign key (study_course_id) references study_course;
