9a5d4359d9999efa39beda1c690feb669f4129b6
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-09-09 11:32:01 +0200

    constraints tag name   migration skript

--- src/main/java/io/archilab/prox/projectservice/tags/TagName.java
+++ src/main/java/io/archilab/prox/projectservice/tags/TagName.java
@@ -1,7 +1,7 @@
 package io.archilab.prox.projectservice.tags;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 import io.archilab.prox.projectservice.project.ProjectDescription;
 import lombok.AccessLevel;
@@ -15,11 +15,23 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TagName {
 	
+    private static final int MAX_LENGTH = 40;
+	
+	@Column(length = 40)
 	public String tagName;
 	
 	public TagName(String tagName)
 	{
-		this.tagName = tagName;
+		if (!ProjectDescription.isValid(tagName)) {
+		      throw new IllegalArgumentException(
+		          String.format("Name %s exceeded maximum number of %d allowed characters", tagName,
+		        		  TagName.MAX_LENGTH));
+		    }
+		this.tagName = tagName.toLowerCase();
 	}
+	
+	public static boolean isValid(String tagName) {
+	    return tagName != null && tagName.length() <= TagName.MAX_LENGTH;
+	  }
 
 }
