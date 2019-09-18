56585f8c403a0d082939ea8d883083a2348005fe
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-09-18 10:15:39 +0200

    test link add

--- src/main/java/io/archilab/prox/projectservice/config/RestConfig.java
+++ src/main/java/io/archilab/prox/projectservice/config/RestConfig.java
@@ -1,5 +1,7 @@
 package io.archilab.prox.projectservice.config;
 
+import java.net.MalformedURLException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
 
@@ -14,6 +16,11 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 import io.archilab.prox.projectservice.project.Project;
 
@@ -27,6 +34,12 @@ public class RestConfig implements RepositoryRestConfigurer {
   
   @Autowired
   private Environment env;
+  
+  private final EurekaClient eurekaClient;
+
+  public RestConfig(EurekaClient eurekaClient) {
+    this.eurekaClient = eurekaClient;
+  }
 
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -44,10 +57,28 @@ public class RestConfig implements RepositoryRestConfigurer {
        public Resource<Project> process(Resource<Project> resource) {
       	 
       	String projectID = resource.getContent().getId().toString();
+
+      	UriComponents request  = ServletUriComponentsBuilder.fromCurrentRequest().build();
+        String scheme = request.getScheme() + "://";
+        String serverName = request.getHost();
+        String serverPort = "";
+        serverPort = ""+request.getPort();
+     //   serverPort = (request.getPort() == 80) ? "" : ":" + request.getPort();
+//        String contextPath = request.getPath();
+        
+//        log.info(scheme + serverName + serverPort); 
+        
+        log.info(serviceUrl("project-service"));
       	
-        resource.add(new Link(env.getProperty("tagServiceLink.tag-collection")+"/"+projectID, "tagCollection"));
+      	
+        resource.add(new Link(scheme + serverName + serverPort+"/"+env.getProperty("tagServiceLink.tag-collection")+"/"+projectID, "tagCollection"));
         return resource;
        }
      };
+  }
+  
+  private String serviceUrl(String service) {
+    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka(service, false);
+    return instance.getHomePageUrl();
   }
 }
