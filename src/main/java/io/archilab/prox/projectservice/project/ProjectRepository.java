d61bfb560beae726d32eb04c62682d6293155917
Florian Baldus <florian.baldus@t-online.de>
2019-10-01 10:55:12 +0200

    Project search: findAllByModifiedAfter

--- src/main/java/io/archilab/prox/projectservice/project/ProjectRepository.java
+++ src/main/java/io/archilab/prox/projectservice/project/ProjectRepository.java
@@ -1,9 +1,11 @@
 package io.archilab.prox.projectservice.project;
 
+import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public interface ProjectRepository extends PagingAndSortingRepository<Project, UUID> {
 
@@ -11,4 +13,9 @@ public interface ProjectRepository extends PagingAndSortingRepository<Project, U
 
   Set<Project> findBySupervisorName_SupervisorNameContaining(
       @Param(value = "supervisorName") String supervisorName);
+
+  Set<Project> findAllByModifiedAfter(
+          @DateTimeFormat(pattern = "yyyy-MM-dd")
+          @Param(value = "modified") Date modified);
+
 }
