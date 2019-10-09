53bcfe913fb9cc3c65fd2085608ae22f4646a313
JannIntveen <JannIntveen@gmail.com>
2019-10-09 13:13:43 +0200

    bugfix

--- src/main/java/io/archilab/prox/projectservice/module/StudyCourseService.java
+++ src/main/java/io/archilab/prox/projectservice/module/StudyCourseService.java
@@ -35,6 +35,9 @@ public class StudyCourseService {
   public void importStudyCourses() {
     this.logger.info("Start importing Study Courses");
 
+    this.studyCourseRepository.deleteAll();
+    this.moduleRepository.deleteAll();
+
     List<StudyCourse> studyCourses = this.studyCourseClient.getStudyCourses();
     for (StudyCourse studyCourse : studyCourses) {
       Optional<StudyCourse> existingStudyCourseOptional = this.studyCourseRepository
