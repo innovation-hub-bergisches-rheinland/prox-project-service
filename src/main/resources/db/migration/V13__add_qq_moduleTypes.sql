6e1fc68ce9468ab8b90b27b6289e2189e8c8c748
Tobias Hund <tobias.hund@innovation-hub.de>
2021-04-20 16:43:10 +0200

    Add migration of QQ modules

---
+++ src/main/resources/db/migration/V13__add_qq_moduleTypes.sql
@@ -0,0 +1,12 @@
+INSERT INTO module_type(id, key, name) VALUES ('4aa09f72-f832-4704-b26b-9af2502c80e0', 'QQ1', 'QQ 1');
+INSERT INTO module_type(id, key, name) VALUES ('097d8f02-268a-4d8c-9a92-998b0539f2b3', 'QQ2', 'QQ 2');
+
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('002997c3-7fcb-4361-868a-cb6530326096', '4aa09f72-f832-4704-b26b-9af2502c80e0');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('fd3d9641-e8dd-4e4a-b17f-3d3d36159c5a', '4aa09f72-f832-4704-b26b-9af2502c80e0');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('ecfad8e5-34fe-43c6-9336-c7d52c957a3e', '4aa09f72-f832-4704-b26b-9af2502c80e0');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('e58de578-19be-4cf9-b6d0-605c197dd9ee', '4aa09f72-f832-4704-b26b-9af2502c80e0');
+
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('002997c3-7fcb-4361-868a-cb6530326096', '097d8f02-268a-4d8c-9a92-998b0539f2b3');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('fd3d9641-e8dd-4e4a-b17f-3d3d36159c5a', '097d8f02-268a-4d8c-9a92-998b0539f2b3');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('ecfad8e5-34fe-43c6-9336-c7d52c957a3e', '097d8f02-268a-4d8c-9a92-998b0539f2b3');
+INSERT INTO study_program_modules(study_program_id, modules_id) VALUES ('e58de578-19be-4cf9-b6d0-605c197dd9ee', '097d8f02-268a-4d8c-9a92-998b0539f2b3');
