8e722f62ddec84004240ea63b531c0be600ff485
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-01-14 22:47:21 +0100

    fix: add missing projection annotation

--- src/main/java/de/innovationhub/prox/projectservice/module/projection/StudyProgramWithModulesProjection.java
+++ src/main/java/de/innovationhub/prox/projectservice/module/projection/StudyProgramWithModulesProjection.java
@@ -5,6 +5,7 @@ import de.innovationhub.prox.projectservice.module.StudyProgram;
 import java.util.Set;
 import org.springframework.data.rest.core.config.Projection;
 
+@Projection(name = "withModules", types = {StudyProgram.class})
 public interface StudyProgramWithModulesProjection {
   String getKey();
   String getName();
