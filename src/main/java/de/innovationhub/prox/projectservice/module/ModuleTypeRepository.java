ddaaa02a3a7131f7c4cc11938fdd9b0877f47734
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-20 16:43:00 +0100

    feat: add query methods for specializations based on modules

--- src/main/java/de/innovationhub/prox/projectservice/module/ModuleTypeRepository.java
+++ src/main/java/de/innovationhub/prox/projectservice/module/ModuleTypeRepository.java
@@ -1,11 +1,25 @@
 package de.innovationhub.prox.projectservice.module;
 
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Set;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Tag(name = "modules")
 @RepositoryRestResource
-public interface ModuleTypeRepository extends CrudRepository<ModuleType, UUID> {}
+public interface ModuleTypeRepository extends CrudRepository<ModuleType, UUID> {
+
+  @Query("select m from StudyProgram s join s.modules m where s.specialization.id = :id")
+  Set<ModuleType> findAllModuleTypesOfSpecializationId(@Param("id") UUID id);
+
+  @Query("select s from StudyProgram sp join sp.specialization s join sp.modules mt where mt.id IN :ids")
+  Set<Specialization> findSpecializationsOfModules(
+      @Param("ids") @Parameter(array = @ArraySchema(uniqueItems = true, schema = @Schema(name = "string", type = "uuid"))) Set<UUID> ids);
+}
---
+++ src/test/java/de/innovationhub/prox/projectservice/module/ModuleTypeRepositoryTest.java
@@ -0,0 +1,88 @@
+package de.innovationhub.prox.projectservice.module;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureDataJpa
+@Transactional
+class ModuleTypeRepositoryTest {
+
+  @Autowired
+  ModuleTypeRepository moduleTypeRepository;
+
+  @Autowired
+  EntityManager entityManager;
+
+  @Test
+  void findAllModuleTypesOfSpecializationId() {
+    // Given
+    var specialization = new Specialization("TST", "Test Specialization");
+    var moduleTypesOfSpecialization = Set.of(
+        new ModuleType("T1", "Test 1"),
+        new ModuleType("T2", "Test 2"),
+        new ModuleType("T3", "Test 3")
+    );
+    var otherModuleTypes = Set.of(
+        new ModuleType("T4", "Test 4"),
+        new ModuleType("T5", "Test 5"),
+        new ModuleType("T6", "Test 6")
+    );
+    var studyProgram = new StudyProgram("TST", "Test StudyProgram", moduleTypesOfSpecialization,
+        specialization);
+    studyProgram.setModules(moduleTypesOfSpecialization);
+
+    entityManager.persist(specialization);
+    moduleTypesOfSpecialization.forEach(entityManager::persist);
+    otherModuleTypes.forEach(entityManager::persist);
+    entityManager.persist(studyProgram);
+    entityManager.flush();
+
+    // When
+    var result = moduleTypeRepository.findAllModuleTypesOfSpecializationId(specialization.getId());
+
+    // Then
+    assertThat(result).containsExactlyInAnyOrderElementsOf(moduleTypesOfSpecialization);
+  }
+
+  @Test
+  void findSpecializationsOfModules() {
+    // Given
+    var specialization = new Specialization("TST", "Test Specialization");
+    var moduleTypesOfSpecialization = Set.of(
+        new ModuleType("T1", "Test 1"),
+        new ModuleType("T2", "Test 2"),
+        new ModuleType("T3", "Test 3")
+    );
+    var otherModuleTypes = Set.of(
+        new ModuleType("T4", "Test 4"),
+        new ModuleType("T5", "Test 5"),
+        new ModuleType("T6", "Test 6")
+    );
+    var studyProgram = new StudyProgram("TST", "Test StudyProgram", moduleTypesOfSpecialization,
+        specialization);
+    studyProgram.setModules(moduleTypesOfSpecialization);
+
+    entityManager.persist(specialization);
+    moduleTypesOfSpecialization.forEach(entityManager::persist);
+    otherModuleTypes.forEach(entityManager::persist);
+    entityManager.persist(studyProgram);
+    entityManager.flush();
+
+    // When
+    var result = moduleTypeRepository.findSpecializationsOfModules(
+        moduleTypesOfSpecialization.stream().map(ModuleType::getId).collect(Collectors.toSet()));
+
+    // Then
+    assertThat(result).hasSize(1)
+        .containsExactly(specialization);
+  }
+}
