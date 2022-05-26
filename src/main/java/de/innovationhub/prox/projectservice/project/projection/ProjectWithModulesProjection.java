447fccbfe96290b0fb35c8fefc93d6eebd8c2906
twobiers <22715034+twobiers@users.noreply.github.com>
2022-05-26 11:35:53 +0200

    remove obsolet context

--- src/main/java/de/innovationhub/prox/projectservice/project/projection/ProjectWithModulesProjection.java
+++ src/main/java/de/innovationhub/prox/projectservice/project/projection/ProjectWithModulesProjection.java
@@ -3,8 +3,8 @@ package de.innovationhub.prox.projectservice.project.projection;
 
 import de.innovationhub.prox.projectservice.module.ModuleType;
 import de.innovationhub.prox.projectservice.module.Specialization;
+import de.innovationhub.prox.projectservice.owners.AbstractOwner;
 import de.innovationhub.prox.projectservice.project.Project;
-import de.innovationhub.prox.projectservice.project.ProjectContext;
 import de.innovationhub.prox.projectservice.project.ProjectStatus;
 import java.time.Instant;
 import java.util.Set;
@@ -28,7 +28,7 @@ public interface ProjectWithModulesProjection {
 
   ProjectStatus getStatus();
 
-  ProjectContext getContext();
+  AbstractOwner getOwner();
 
   UUID getCreatorID();
 
