dcd87feaf7a3757e0664bd32f7ddc00c995a6b64
twobiers <22715034+twobiers@users.noreply.github.com>
2022-05-26 13:58:38 +0200

    refactor: add excerpt projection for study program

--- src/main/java/de/innovationhub/prox/projectservice/module/StudyProgramRepository.java
+++ src/main/java/de/innovationhub/prox/projectservice/module/StudyProgramRepository.java
@@ -1,12 +1,13 @@
 package de.innovationhub.prox.projectservice.module;
 
 
+import de.innovationhub.prox.projectservice.module.projection.StudyProgramWithModulesProjection;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Tag(name = "studyPrograms")
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = StudyProgramWithModulesProjection.class)
 public interface StudyProgramRepository
     extends CrudRepository<StudyProgram, UUID>, StudyProgramRepositoryCustom {}
