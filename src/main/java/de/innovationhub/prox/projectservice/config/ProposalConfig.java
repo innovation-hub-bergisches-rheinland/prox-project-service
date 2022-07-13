efd1845ef1ff61a883fa079b67ad0426b5111941
Tobias Hund <tobias.hund@innovation-hub.de>
2022-07-13 21:05:33 +0200

    refactor: rework configuration

--- src/main/java/de/innovationhub/prox/projectservice/config/ProposalConfig.java
+++ src/main/java/de/innovationhub/prox/projectservice/config/ProposalConfig.java
@@ -4,49 +4,103 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.boot.convert.DurationUnit;
+import org.springframework.stereotype.Component;
 
-// TODO: Refactor, Generate Metadata and try to use records
-@Getter
+/*@Getter
 @ConfigurationProperties(prefix = "project-service.proposals")
 public class ProposalConfig {
-
-  /**
-   * Enable the auto archiving of proposals.
-   */
-  private final Boolean enableAutoArchive;
-  /**
-   * Time after last status change to archive a proposal in days.
-   */
-  @DurationUnit(ChronoUnit.DAYS)
-  private final Duration autoArchiveAfter;
-  /**
-   * Enable auto marking for deletion of proposals.
-   */
-  private final Boolean enableAutoMarkForDeletion;
-  /**
-   * Time after last status change to mark a proposal for deletion in days.
-   */
-  @DurationUnit(ChronoUnit.DAYS)
-  private final Duration autoMarkDeletionAfter;
-  /**
-   * Enable auto deletion of proposals.
-   */
-  private final Boolean enableAutoDeletion;
+  private final ProposalJobConfig jobs;
 
   @ConstructorBinding
-  public ProposalConfig(
-      @DefaultValue("true") Boolean enableAutoArchive,
-      @DurationUnit(ChronoUnit.DAYS) @DefaultValue("90") Duration autoArchiveAfter,
-      @DefaultValue("true") Boolean enableAutoMarkForDeletion,
-      @DurationUnit(ChronoUnit.DAYS) @DefaultValue("30") Duration autoMarkDeletionAfter,
-      @DefaultValue("true") Boolean enableAutoDeletion) {
-    this.enableAutoArchive = enableAutoArchive;
-    this.autoArchiveAfter = autoArchiveAfter;
-    this.enableAutoMarkForDeletion = enableAutoMarkForDeletion;
-    this.autoMarkDeletionAfter = autoMarkDeletionAfter;
-    this.enableAutoDeletion = enableAutoDeletion;
+  public ProposalConfig(@DefaultValue ProposalJobConfig jobs) {
+    this.jobs = jobs;
+  }
+
+  @Getter
+  public class ProposalJobConfig {
+    private final AutoArchiveConfig autoArchive;
+    private final AutoMarkForDeletionConfig autoMarkForDelete;
+    private final AutoDeletionConfig autoDelete;
+
+    public ProposalJobConfig(@DefaultValue AutoArchiveConfig autoArchive,
+        @DefaultValue AutoMarkForDeletionConfig autoMarkForDelete, @DefaultValue AutoDeletionConfig autoDelete) {
+      this.autoArchive = autoArchive;
+      this.autoMarkForDelete = autoMarkForDelete;
+      this.autoDelete = autoDelete;
+    }
+
+    @Getter
+    public class AutoArchiveConfig {
+      private final Boolean enable;
+      private final Duration after;
+      private final String cron;
+
+      public AutoArchiveConfig(
+          @DefaultValue("true") Boolean enable,
+          @DurationUnit(ChronoUnit.DAYS) @DefaultValue("90") Duration after,
+          @DefaultValue("0 0 0 * * *") String cron) {
+        this.enable = enable;
+        this.after = after;
+        this.cron = cron;
+      }
+    }
+
+    @Getter
+    public class AutoMarkForDeletionConfig {
+      private final Boolean enable;
+      private final Duration after;
+      private final String cron;
+      public AutoMarkForDeletionConfig(
+          @DefaultValue("true") Boolean enable,
+          @DurationUnit(ChronoUnit.DAYS) @DefaultValue("30") Duration after,
+          @DefaultValue("0 0 0 * * *") String cron) {
+        this.enable = enable;
+        this.after = after;
+        this.cron = cron;
+      }
+    }
+
+    @Getter
+    public class AutoDeletionConfig {
+      private final Boolean enable;
+      private final String cron;
+      public AutoDeletionConfig(
+          @DefaultValue("true") Boolean enable,
+          @DefaultValue("0 0 0 * * *") String cron) {
+        this.enable = enable;
+        this.cron = cron;
+      }
+    }
+  }
+}*/
+
+@ConfigurationProperties(prefix = "project-service.proposals")
+public record ProposalConfig(@DefaultValue ProposalJobConfig jobs) {
+
+  public record ProposalJobConfig(@DefaultValue AutoArchiveConfig autoArchive,
+                                  @DefaultValue AutoMarkForDeletionConfig autoMarkForDelete,
+                                  @DefaultValue AutoDeletionConfig autoDelete) {
+
+    public record AutoArchiveConfig(@DefaultValue("true") Boolean enable,
+                                    @DurationUnit(ChronoUnit.DAYS) @DefaultValue("90") Duration after
+                                    // @DefaultValue("0 0 0 * * *") String cron
+    ) {
+
+    }
+
+    public record AutoMarkForDeletionConfig(@DefaultValue("true") Boolean enable,
+                                            @DurationUnit(ChronoUnit.DAYS) @DefaultValue("90") Duration after
+                                            // @DefaultValue("0 0 0 * * *") String cron
+    ) {
+
+    }
+
+    public record AutoDeletionConfig(@DefaultValue("true") Boolean enable
+                                     // @DefaultValue("0 0 0 * * *") String cron
+    ) {
+
+    }
   }
 }
