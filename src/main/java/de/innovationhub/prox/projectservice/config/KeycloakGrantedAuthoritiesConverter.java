0913befb48488f458f4c144ee98585f09fe169ea
Tobias Hund <tobias.hund@innovation-hub.de>
2022-07-23 11:34:45 +0200

    chore: introduce feature flags as granted authority

--- src/main/java/de/innovationhub/prox/projectservice/config/KeycloakGrantedAuthoritiesConverter.java
+++ src/main/java/de/innovationhub/prox/projectservice/config/KeycloakGrantedAuthoritiesConverter.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,9 +17,23 @@ public class KeycloakGrantedAuthoritiesConverter
   @Override
   public Collection<GrantedAuthority> convert(Jwt source) {
     Map<String, Object> realmAccess = source.getClaimAsMap("realm_access");
-    List<String> roles = (List<String>) realmAccess.get("roles");
-    return roles.stream()
-        .map(rn -> new SimpleGrantedAuthority("ROLE_" + rn))
-        .collect(Collectors.toList());
+
+    var roles = (List<String>) realmAccess.get("roles");
+    Stream<GrantedAuthority> roleAuthorities = Stream.empty();
+    if (roles != null) {
+      roleAuthorities = roles.stream()
+          .map(rn -> new SimpleGrantedAuthority("ROLE_" + rn));
+    }
+
+
+    var features = source.getClaimAsStringList("features");
+    Stream<GrantedAuthority> featureAuthorities = Stream.empty();
+    if(features != null) {
+      featureAuthorities = features.stream()
+          .map(fn -> new SimpleGrantedAuthority("FEATURE_" + fn));
+    }
+
+
+    return Stream.concat(roleAuthorities, featureAuthorities).collect(Collectors.toList());
   }
 }
