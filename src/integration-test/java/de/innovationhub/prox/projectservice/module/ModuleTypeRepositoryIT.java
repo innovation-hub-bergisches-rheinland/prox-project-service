5a93f1f16e330bd49ca9a4d54723917fbff3f2a0
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-09 18:37:56 +0200

    test: introduce helper methods

--- src/integration-test/java/de/innovationhub/prox/projectservice/module/ModuleTypeRepositoryIT.java
+++ src/integration-test/java/de/innovationhub/prox/projectservice/module/ModuleTypeRepositoryIT.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import de.innovationhub.prox.projectservice.AbstractDatabaseIT;
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,69 +15,52 @@ class ModuleTypeRepositoryIT extends AbstractDatabaseIT {
   @Autowired EntityManager entityManager;
 
   @Test
-  void findAllModuleTypesOfSpecializationId() {
-    // Given
-    var specialization = new Specialization("TST", "Test Specialization");
-    var moduleTypesOfSpecialization =
-        Set.of(
-            new ModuleType("T1", "Test 1"),
-            new ModuleType("T2", "Test 2"),
-            new ModuleType("T3", "Test 3"));
-    var otherModuleTypes =
-        Set.of(
-            new ModuleType("T4", "Test 4"),
-            new ModuleType("T5", "Test 5"),
-            new ModuleType("T6", "Test 6"));
-    var studyProgram =
-        new StudyProgram("TST", "Test StudyProgram", moduleTypesOfSpecialization, specialization);
-    studyProgram.setModules(moduleTypesOfSpecialization);
+  void shouldFindModuleTypesOfSpecializationId() {
+    var specialization = createAndPersistSpecialization();
+    var moduleTypes = Set.of(
+      createAndPersistModuleType("TST1", "Test Module Type 1")
+    );
+    createAndPersistStudyProgram(moduleTypes, specialization);
 
-    entityManager.persist(specialization);
-    moduleTypesOfSpecialization.forEach(entityManager::persist);
-    otherModuleTypes.forEach(entityManager::persist);
-    entityManager.persist(studyProgram);
-    entityManager.flush();
+    var result = moduleTypeRepository.findAllModuleTypesOfSpecializationId(Set.of("TST"));
 
-    // When
-    var result =
-        moduleTypeRepository.findAllModuleTypesOfSpecializationId(Set.of(specialization.getKey()));
-
-    // Then
-    assertThat(result).containsExactlyInAnyOrderElementsOf(moduleTypesOfSpecialization);
+    assertThat(result)
+      .containsExactlyInAnyOrderElementsOf(moduleTypes);
   }
 
   @Test
-  void findSpecializationsOfModules() {
-    // Given
-    var specialization = new Specialization("TST", "Test Specialization");
-    var moduleTypesOfSpecialization =
+  void shouldFindSpecializationsOfModule() {
+    var specialization = createAndPersistSpecialization();
+    var moduleTypes =
         Set.of(
-            new ModuleType("T1", "Test 1"),
-            new ModuleType("T2", "Test 2"),
-            new ModuleType("T3", "Test 3"));
-    var otherModuleTypes =
-        Set.of(
-            new ModuleType("T4", "Test 4"),
-            new ModuleType("T5", "Test 5"),
-            new ModuleType("T6", "Test 6"));
-    var studyProgram =
-        new StudyProgram("TST", "Test StudyProgram", moduleTypesOfSpecialization, specialization);
-    studyProgram.setModules(moduleTypesOfSpecialization);
+            createAndPersistModuleType("T1", "Test 1")
+        );
 
-    entityManager.persist(specialization);
-    moduleTypesOfSpecialization.forEach(entityManager::persist);
-    otherModuleTypes.forEach(entityManager::persist);
-    entityManager.persist(studyProgram);
-    entityManager.flush();
+    createAndPersistStudyProgram(moduleTypes, specialization);
 
-    // When
     var result =
-        moduleTypeRepository.findSpecializationsOfModules(
-            moduleTypesOfSpecialization.stream()
-                .map(ModuleType::getKey)
-                .collect(Collectors.toSet()));
+        moduleTypeRepository.findSpecializationsOfModules(Set.of("T1"));
 
-    // Then
-    assertThat(result).hasSize(1).containsExactly(specialization);
+    assertThat(result)
+      .containsExactly(specialization);
+  }
+
+  private Specialization createAndPersistSpecialization() {
+    var specialization = new Specialization("TST", "Test Specialization");
+    entityManager.persist(specialization);
+    return specialization;
+  }
+
+  private ModuleType createAndPersistModuleType(String key, String name) {
+    var moduleType = new ModuleType(key, name);
+    entityManager.persist(moduleType);
+    return moduleType;
+  }
+
+  private StudyProgram createAndPersistStudyProgram(
+      Set<ModuleType> moduleTypes, Specialization specialization) {
+    var studyProgram = new StudyProgram("TST", "Test StudyProgram", moduleTypes, specialization);
+    entityManager.persist(studyProgram);
+    return studyProgram;
   }
 }
--- src/integration-test/java/de/innovationhub/prox/projectservice/module/StudyProgramRepositoryIT.java
+++ src/integration-test/java/de/innovationhub/prox/projectservice/module/StudyProgramRepositoryIT.java
@@ -19,14 +19,11 @@ class StudyProgramRepositoryIT extends AbstractDatabaseIT {
 
   @Test
   void shouldReturnModule_findAllModulesOfStudyPrograms() {
-    var moduleType = new ModuleType("TST", "Test");
-    em.persist(moduleType);
-    var specialization = new Specialization("TST", "Test");
-    em.persist(specialization);
-    var studyProgram1 = new StudyProgram("TST", "Test", Set.of(moduleType), specialization);
-    em.persist(studyProgram1);
+    var moduleType = createAndPersistModuleType();
+    var specialization = createAndPersistSpecialization();
+    var studyProgram = createAndPersistStudyProgram("TST", Set.of(moduleType), specialization);
 
-    var result = studyProgramRepository.findAllModulesOfStudyPrograms(List.of(studyProgram1.getId()));
+    var result = studyProgramRepository.findAllModulesOfStudyPrograms(List.of(studyProgram.getId()));
 
     assertThat(result)
       .hasSize(1)
@@ -36,18 +33,34 @@ class StudyProgramRepositoryIT extends AbstractDatabaseIT {
 
   @Test
   void shouldRemoveDuplicatesOn_findAllModulesOfStudyPrograms() {
-    var moduleType = new ModuleType("TST", "Test");
-    em.persist(moduleType);
-    var specialization = new Specialization("TST", "Test");
-    em.persist(specialization);
-    var studyProgram1 = new StudyProgram("TST", "Test", Set.of(moduleType), specialization);
-    var studyProgram2 = new StudyProgram("TST2", "Test 2", Set.of(moduleType), specialization);
-    em.persist(studyProgram1);
-    em.persist(studyProgram2);
+    var moduleType = createAndPersistModuleType();
+    var specialization = createAndPersistSpecialization();
+    var studyProgram1 = createAndPersistStudyProgram("TST", Set.of(moduleType), specialization);
+    var studyProgram2 = createAndPersistStudyProgram("TST2", Set.of(moduleType), specialization);
 
-    var result = studyProgramRepository.findAllModulesOfStudyPrograms(List.of(studyProgram1.getId(), studyProgram2.getId()));
+    var result = studyProgramRepository
+      .findAllModulesOfStudyPrograms(List.of(studyProgram1.getId(), studyProgram2.getId()));
 
     assertThat(result)
       .hasSize(1);
+  }
+
+  private Specialization createAndPersistSpecialization() {
+    var specialization = new Specialization("TST", "Test Specialization");
+    em.persist(specialization);
+    return specialization;
+  }
+
+  private ModuleType createAndPersistModuleType() {
+    var moduleType = new ModuleType("TST", "Test");
+    em.persist(moduleType);
+    return moduleType;
+  }
+
+  private StudyProgram createAndPersistStudyProgram(String key,
+    Set<ModuleType> moduleTypes, Specialization specialization) {
+    var studyProgram = new StudyProgram(key, "Test StudyProgram", moduleTypes, specialization);
+    em.persist(studyProgram);
+    return studyProgram;
   }
 }
