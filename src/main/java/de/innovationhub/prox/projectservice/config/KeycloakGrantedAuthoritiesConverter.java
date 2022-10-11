a7487e8fb3dbb4181d108d72ed1d97aa659459f8
Tobias Hund <tobias.hund@innovation-hub.de>
2022-10-11 11:48:46 +0200

    refactor: collect immutable list

--- src/main/java/de/innovationhub/prox/projectservice/config/KeycloakGrantedAuthoritiesConverter.java
+++ src/main/java/de/innovationhub/prox/projectservice/config/KeycloakGrantedAuthoritiesConverter.java
@@ -4,7 +4,6 @@ package de.innovationhub.prox.projectservice.config;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
@@ -37,6 +36,6 @@ public class KeycloakGrantedAuthoritiesConverter
       featureAuthorities = features.stream().map(fn -> new SimpleGrantedAuthority("FEATURE_" + fn));
     }
 
-    return Stream.concat(roleAuthorities, featureAuthorities).collect(Collectors.toList());
+    return Stream.concat(roleAuthorities, featureAuthorities).toList();
   }
 }
